b207acdeeb48793e4a381e8fbd4e895273d98e2e
Peddn <public@peddn.net>
2025-08-23 14:14:30 +0200

    Update default_cmdsets.py

---
+++ commands/de_commands/general.py
@@ -0,0 +1,26 @@
+from evennia.default_cmds import CmdHome
+
+class CmdHomeDE(CmdHome):
+    """
+    Bewege dich zur dem Ort, den dein Charakter als Zuhause gesetzt hat.
+
+    Usage:
+      heim
+
+    Teleportiert dich nach Hause.
+    """
+
+    key = "heim"
+    aliases = ["home"]
+
+    def func(self):
+        """Kopie von CmdHome.func, aber mit deutscher Ausgabe."""
+        caller = self.caller
+        home = caller.home
+        if not home:
+            caller.msg("Du hast kein Zuhause!")
+        elif home == caller.location:
+            caller.msg("Du bist doch bereits Zuhause!")
+        else:
+            caller.msg("Zuhause ist es am schönsten...")
+            caller.move_to(home, move_type="teleport")
